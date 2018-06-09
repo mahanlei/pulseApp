c26a56182c4e3f64bd3c3662285ad36c1ab02b99
HaileyMa <1216832052@qq.com>
2018-06-09 20:02:29 +0800

    蓝牙模块代码没加上

--- app/src/main/java/com/example/apple/pluseapp/activity/ShowActivity.java
+++ app/src/main/java/com/example/apple/pluseapp/activity/ShowActivity.java
@@ -2,21 +2,24 @@ package com.example.apple.pluseapp.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.apple.pluseapp.R;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.Entry;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShowActivity extends AppCompatActivity {
-
+public class ShowActivity extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+        findViewById(R.id.connBt).setOnClickListener(this);
+
       LineChart mLineChart=findViewById(R.id.chart);
 //显示边界
         mLineChart.setDrawBorders(false);
@@ -32,4 +35,14 @@ public class ShowActivity extends AppCompatActivity {
         LineData data = new LineData(lineDataSet);
         mLineChart.setData(data);
     }
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/apple/pluseapp/util/BlueTooth.java
@@ -0,0 +1,29 @@
+package com.example.apple.pluseapp.util;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+
+public class BlueTooth {
+//    public static final int REQUEST_BT_ENABLE_CODE = 200;
+//    private BluetoothAdapter mBluetoothAdapter;//蓝牙适配器
+//    //开启蓝牙
+//    private void openBT() {
+//        if (mBluetoothAdapter == null) {
+//            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+//        }
+//        //1.设备不支持蓝牙，结束应用
+//        if (mBluetoothAdapter == null) {
+//            finish();
+//            return;
+//        }
+//        //2.判断蓝牙是否打开
+//        if (!mBluetoothAdapter.enable()) {
+//            //没打开请求打开
+//            Intent btEnable = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(btEnable, REQUEST_BT_ENABLE_CODE);
+//        }
+//    }
+
+
+
+}
